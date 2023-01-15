575c7b57ddcdb26f4b69b9c308e6a33c82b5038e
Ivan Kalagin <lulldev@yandex.ru>
2023-01-15 18:17:07 +0700

    fix: update build in vcs

--- dist/core/index.d.ts
+++ dist/core/index.d.ts
@@ -11,6 +11,9 @@ export type RemoteDataNotAsked = {
 export type RemoteDataLoading = {
     type: 'LOADING';
 };
+export type RemoteDataReloading = {
+    type: 'RELOADING';
+};
 export type RemoteDataFailure<E> = {
     type: 'FAILURE';
     error: E;
@@ -19,12 +22,16 @@ export type RemoteDataSuccess<D = unknown> = {
     type: 'SUCCESS';
     data: D;
 };
-export type RemoteData<E, D> = RemoteDataNotAsked | RemoteDataLoading | RemoteDataFailure<E> | RemoteDataSuccess<D>;
+export type RemoteData<E, D> = RemoteDataNotAsked | RemoteDataLoading | RemoteDataReloading | RemoteDataFailure<E> | RemoteDataSuccess<D>;
 export type FoldHandlers<E, D, R> = {
     notAsked: () => R;
     success: (data: D) => R;
     loading?: () => R;
+    reloading?: () => R;
     failure?: (error: E) => R;
+};
+export type FolderHandlersRequired<E, D, R> = Required<Omit<FoldHandlers<E, D, R>, 'reloading'>> & {
+    reloading?: () => R;
 };
 export type FoldHandlersA<RDS, R> = {
     notAsked: () => R;
@@ -33,22 +40,271 @@ export type FoldHandlersA<RDS, R> = {
         data: unknown;
     }>['data'][]) => R;
     loading?: () => R;
+    reloading?: () => R;
     failure?: (error: Extract<RDS, {
         type: 'FAILURE';
         error: unknown;
     }>['error'][]) => R;
 };
+export type FolderHandlersARequired<RDS, R> = Required<Omit<FoldHandlersA<RDS, R>, 'reloading'>> & {
+    reloading?: () => R;
+};
+/**
+ * Return NOT_ASKED state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.notAsked();
+ */
 export declare const notAsked: () => RemoteDataNotAsked;
+/**
+ * Return LOADING state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.loading();
+ */
 export declare const loading: () => RemoteDataLoading;
+/**
+ * Return RELOADING state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.reloading();
+ */
+export declare const reloading: () => RemoteDataReloading;
+/**
+ * Return SUCCESS state with data
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.success({ userId: 1000 });
+ */
 export declare const success: <D>(data: D) => RemoteDataSuccess<D>;
+/**
+ * Return FAILURE state with error
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.failure(new Error('error'));
+ */
 export declare const failure: <E>(error: E) => RemoteDataFailure<E>;
+/**
+ * Return true if remoteData is notAsked, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.notAsked();
+ *   if (RD.isNotAsked(data)) {
+ *   // ...your code
+ *   }
+ */
+export declare function isNotAsked<R extends RemoteData<unknown, unknown>>(remoteData: R): boolean;
+/**
+ * Return true if one of remoteData is notAsked, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data1 = RD.notAsked();
+ *   const data2 = RD.loading();
+ *
+ *   if (RD.isNotAsked([data1, data2])) {
+ *   // ...your code
+ *   }
+ */
+export declare function isNotAsked<R extends ArrayTwoOrMore<RemoteData<unknown, unknown>>>(remoteData: R): boolean;
+/**
+ * Return true if remoteData is loading, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.loading();
+ *   if (RD.isLoading(data)) {
+ *   // ...your code
+ *   }
+ */
 export declare function isLoading<R extends RemoteData<unknown, unknown>>(remoteData: R): boolean;
+/**
+ * Return true if one of remoteData is loading, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data1 = RD.notAsked();
+ *   const data2 = RD.loading();
+ *
+ *   if (RD.isNotAsked([data1, data2])) {
+ *   // ...your code
+ *   }
+ */
 export declare function isLoading<R extends ArrayTwoOrMore<RemoteData<unknown, unknown>>>(remoteData: R): boolean;
+/**
+ * Return true if remoteData is reloading, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.reloading();
+ *   if (RD.isReloading(data)) {
+ *   // ...your code
+ *   }
+ */
+export declare function isReloading<R extends RemoteData<unknown, unknown>>(remoteData: R): boolean;
+/**
+ * Return true if one of remoteData is reloading, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data1 = RD.notAsked();
+ *   const data2 = RD.reloading();
+ *
+ *   if (RD.reloading([data1, data2])) {
+ *   // ...your code
+ *   }
+ */
+export declare function isReloading<R extends ArrayTwoOrMore<RemoteData<unknown, unknown>>>(remoteData: R): boolean;
+/**
+ * Return true if remoteData is success, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.success(1);
+ *   if (RD.isSuccess(data)) {
+ *   console.log(data.data); // typesave
+ *   }
+ */
 export declare function isSuccess<R extends RemoteData<unknown, unknown>>(remoteData: R): boolean;
+/**
+ * Return true if ALL of remoteData is success, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data1 = RD.success(1);
+ *   const data2 = RD.success(2);
+ *
+ *   if (RD.isSuccess([data1, data2])) {
+ *   console.log(data1.data, data2.data); // typesave
+ *   }
+ */
 export declare function isSuccess<R extends ArrayTwoOrMore<RemoteData<unknown, unknown>>>(remoteData: R): boolean;
+/**
+ * Return true if remoteData is failure, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.failure(new Error('some error'));
+ *   if (RD.isFailure(data)) {
+ *   // ...your code
+ *   }
+ */
 export declare function isFailure<R extends RemoteData<unknown, unknown>>(remoteData: R): boolean;
+/**
+ * Return true if one of remoteData is failure, else false
+ *
+ * @category Guards
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data1 = RD.failure(new Error('some error'));
+ *   const data2 = RD.failure(new Error('some another error'));
+ *
+ *   if (RD.isFailure([data1, data2])) {
+ *   // ...your code
+ *   }
+ */
 export declare function isFailure<R extends ArrayTwoOrMore<RemoteData<unknown, unknown>>>(remoteData: R): boolean;
+/**
+ * Accept remote data and orElse function and if SUCCESS return data, else all orElse
+ *
+ * @category Error handling / accessor
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   // example 1
+ *   const data = RD.notAsked();
+ *   const result = RD.successOrElse(data, () => -1); // -1
+ *
+ *   // example 2
+ *   const data = RD.success(1);
+ *   const result = RD.successOrElse(data, () => -1); // 1
+ */
 export declare function successOrElse<T extends RemoteData<unknown, unknown>, R>(remoteData: T, orElse: () => R): (T extends RemoteDataSuccess<unknown> ? T['data'] : never) | R;
+/**
+ * Accept array of remote data and orElse function and if ALL SUCCESS return array of
+ * data, else all orElse
+ *
+ * @category Error handling / accessor
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   // example 1
+ *   const data1 = RD.notAsked();
+ *   const data2 = RD.notAsked();
+ *   const result = RD.successOrElse([data1, data2], () => [0, 0]); // [0, 0]
+ *
+ *   // example 2
+ *   const data1 = RD.success(1);
+ *   const data2 = RD.success(2);
+ *   const [first, second] = RD.successOrElse(data, () => [0, 0]); // [1, 2]
+ */
 export declare function successOrElse<T extends ArrayTwoOrMore<RemoteData<unknown, unknown>>, R>(remoteData: T, orElse: () => R): (T extends ArrayTwoOrMore<RemoteDataSuccess<unknown>> ? T[number]['data'][] : never) | R;
+/**
+ * Accept remote data and object with handlers (state as cb)
+ *
+ * @category Pattern matching
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   // example 1
+ *   const data = RD.notAsked();
+ *   const result = RD.fold(data, { notAsked: () => 'no data', loading: () => 'loading...', success: (data) => `data: ${data}`);
+ */
 export declare function fold<E, D, R>(remoteData: RemoteData<E, D>, foldHandlers: FoldHandlers<E, D, R>): ReturnTypesOfFunctionProps<R>;
+/**
+ * Accept array of remote data and object with handler (state as cb)
+ *
+ * @category Pattern matching
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   // example 1
+ *   const data1 = RD.notAsked();
+ *   const data2 = RD.notAsked();
+ *
+ *   // return 'no data':
+ *   const result = RD.fold([data1, data2], { notAsked: () => 'no data', loading: () => 'loading...', success: (data) => `data: ${data}`);
+ *
+ *   // example 2
+ *   const data1 = RD.success(1);
+ *   const data2 = RD.success(2);
+ *
+ *   // return 3:
+ *   const result = RD.fold([data1, data2], { notAsked: () => 'no data', loading: () => 'loading...', success: ([a, b]) => a + b);
+ */
 export declare function fold<RDS, R>(remoteData: ArrayTwoOrMore<RDS>, foldHandlers: FoldHandlersA<RDS, R>): ReturnTypesOfFunctionProps<R>;
--- dist/core/index.js
+++ dist/core/index.js
@@ -1,26 +1,85 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.fold = exports.successOrElse = exports.isFailure = exports.isSuccess = exports.isLoading = exports.failure = exports.success = exports.loading = exports.notAsked = void 0;
+exports.fold = exports.successOrElse = exports.isFailure = exports.isSuccess = exports.isReloading = exports.isLoading = exports.isNotAsked = exports.failure = exports.success = exports.reloading = exports.loading = exports.notAsked = void 0;
+/**
+ * Return NOT_ASKED state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.notAsked();
+ */
 var notAsked = function () { return ({ type: 'NOT_ASKED' }); };
 exports.notAsked = notAsked;
+/**
+ * Return LOADING state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.loading();
+ */
 var loading = function () { return ({ type: 'LOADING' }); };
 exports.loading = loading;
+/**
+ * Return RELOADING state
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.reloading();
+ */
+var reloading = function () { return ({ type: 'RELOADING' }); };
+exports.reloading = reloading;
+/**
+ * Return SUCCESS state with data
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.success({ userId: 1000 });
+ */
 var success = function (data) { return ({
     type: 'SUCCESS',
     data: data,
 }); };
 exports.success = success;
+/**
+ * Return FAILURE state with error
+ *
+ * @category Constructors
+ * @example
+ *   import * RD from '@yac/remote-data';
+ *
+ *   const data = RD.failure(new Error('error'));
+ */
 var failure = function (error) { return ({
     type: 'FAILURE',
     error: error,
 }); };
 exports.failure = failure;
+function isNotAsked(remoteData) {
+    return Array.isArray(remoteData)
+        ? remoteData.some(function (rd) { return rd.type === 'NOT_ASKED'; })
+        : remoteData.type === 'NOT_ASKED';
+}
+exports.isNotAsked = isNotAsked;
 function isLoading(remoteData) {
     return Array.isArray(remoteData)
         ? remoteData.some(function (rd) { return rd.type === 'LOADING'; })
         : remoteData.type === 'LOADING';
 }
 exports.isLoading = isLoading;
+function isReloading(remoteData) {
+    return Array.isArray(remoteData)
+        ? remoteData.some(function (rd) { return rd.type === 'RELOADING'; })
+        : remoteData.type === 'RELOADING';
+}
+exports.isReloading = isReloading;
 function isSuccess(remoteData) {
     return Array.isArray(remoteData)
         ? remoteData.every(function (rd) { return rd.type === 'SUCCESS'; })
@@ -46,17 +105,27 @@ function successOrElse(remoteData, orElse) {
 }
 exports.successOrElse = successOrElse;
 function fold(remoteData, _a) {
-    var notAsked = _a.notAsked, loading = _a.loading, failure = _a.failure, success = _a.success;
+    var notAsked = _a.notAsked, loading = _a.loading, failure = _a.failure, success = _a.success, reloading = _a.reloading;
     if (Array.isArray(remoteData)) {
+        if (failure) {
+            var failedRds = remoteData.filter(function (rd) { return isFailure(rd); });
+            if (failedRds.length > 0) {
+                return failure(failedRds.map(function (rd) { return rd.error; }));
+            }
+        }
+        if (loading) {
+            var loadingRds = remoteData.filter(function (rd) { return isLoading(rd); });
+            if (loadingRds.length > 0) {
+                return loading();
+            }
+        }
+        if (reloading) {
+            var reloadingRds = remoteData.filter(function (rd) { return isReloading(rd); });
+            if (reloadingRds.length > 0) {
+                return reloading();
+            }
+        }
         var successRds = remoteData.filter(function (rd) { return isSuccess(rd); });
-        var failedRds = remoteData.filter(function (rd) { return isFailure(rd); });
-        var loadingRds = remoteData.filter(function (rd) { return isLoading(rd); });
-        if (failedRds.length > 0 && failure) {
-            return failure(failedRds.map(function (rd) { return rd.error; }));
-        }
-        if (loadingRds.length > 0 && loading) {
-            return loading();
-        }
         if (remoteData.length === successRds.length) {
             return success(successRds.map(function (rd) { return rd.data; }));
         }
@@ -64,6 +133,9 @@ function fold(remoteData, _a) {
     }
     if (remoteData.type === 'LOADING' && loading) {
         return loading();
+    }
+    if (remoteData.type === 'RELOADING' && reloading) {
+        return reloading();
     }
     if (remoteData.type === 'FAILURE' && failure) {
         return failure(remoteData.error);
